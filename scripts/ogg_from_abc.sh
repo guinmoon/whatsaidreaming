27d0ed41dc87f4bb40e0e06183f5029048af8088
Artem <guinmoon@gmail.com>
2023-01-25 20:48:22 +0300

    ogg from abc

--- scripts/ogg_from_abc.sh
+++ scripts/ogg_from_abc.sh
@@ -1,3 +1,3 @@
 abc2midi $1.abc
 python ../miditools/midisox_py -M $1\1.mid $1\2.mid $1\full.mid
-fluidsynth "A320U.sf2" $1\full.mid -g 2 -i -f progr0.txt -nli -r 48000 -o synth.cpu-cores=8 -T oga -F $1.ogg
+fluidsynth -l -T raw -F - A320U.sf2 $1\full.mid -f progr0.txt -g 2 | lame -b 256 -r - $1.mp3 
