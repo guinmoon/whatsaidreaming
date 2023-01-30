1e45ede1f7bf0cca96789d962c4ebf1446575564
guinmoon <guinmoon@gmail.com>
2023-01-30 14:12:18 +0300

    fix abc

--- scripts/fix_abc.py
+++ scripts/fix_abc.py
@@ -9,6 +9,7 @@ def fix_abc(in_abc):
     abc = abc.replace('minK','\nK:')
     abc = abc.replace('Dmin','D')
     abc = abc.replace('4/42','4/4')
+    abc = abc.replace('::',':')
     return abc
 
 
