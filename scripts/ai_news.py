4b0633871dc215d87aa4dac67eab51771c97c1c0
Artem <guinmoon@gmail.com>
2023-02-01 09:21:44 +0300

    ai news

--- scripts/ai_news.py
+++ scripts/ai_news.py
@@ -2,6 +2,7 @@ import feedparser,os,pickle,json
 from datetime import datetime
 import shutil
 import subprocess
+import glob
 
 from ya_tts_paid import *
 from balaboba_helper import *
@@ -26,7 +27,7 @@ def get_prompt_from_news(feed_url):
 def synth_news_entire(etire):
     a=1
 
-def synt_news(feed_url,max_news_count=5,source_every_x=3):
+def synt_news(feed_url,max_news_count=10,source_every_x=3):    
     ya_conf=None
     think_conf= None
     with open(os.path.join(os.path.dirname(__file__),'ya_cloud_conf.json')) as json_file:
@@ -37,6 +38,7 @@ def synt_news(feed_url,max_news_count=5,source_every_x=3):
     source_tts="Источник: 1777.ru"
     source_tts_file=os.path.join(__dir,'../sounds/source1777.mp3')
     news_dir=os.path.join(__dir,'../news_parts')
+    arch_news(news_dir,os.path.join(__dir,'../news_arch'))
     news_count=0
     tts_to_lpcm(source_tts,f'{news_dir}/tmp_b.pcm',ya_conf['folder_id'],ya_conf['token'],voice='ermil')
     result = subprocess.call(["lame",f'{news_dir}/tmp_b.pcm','-s','24', '--tt', f"{source_tts}", '-b', '256', '-r',f'source.mp3',],cwd=news_dir)
@@ -61,8 +63,22 @@ def synt_news(feed_url,max_news_count=5,source_every_x=3):
     os.remove(f'{news_dir}/tmp_a.pcm')
     os.remove(f'{news_dir}/tmp_b.pcm')
 
-def arch_news():
+def concat_news(news_dir):
     a=1
+
+def arch_news(news_dir,arch_dir):
+    tracks=glob.glob(news_dir+"/*.mp3")
+    # playlist={}
+    # playlist["tracks"]=[]
+    for track in tracks:
+        result = subprocess.call(["mv",track,arch_dir],cwd=__dir)
+        # track_base_name=os.path.basename(track)
+        # c_date=datetime.fromtimestamp(os.path.getmtime(track))
+        # diff_dates = (datetime.today() - c_date)
+        # diff_dates_hours = (diff_dates.days*24 * 60 * 60+diff_dates.seconds)/3600
+        # if diff_dates_hours>=24.0:
+        #     move_to_arch(track)
+        #     continue
 
 if __name__=='__main__':
     synt_news("https://news.1777.ru/rss/yandex")
--- scripts/yandex_tts_free.py
+++ scripts/yandex_tts_free.py
@@ -2,6 +2,8 @@ import http.client
 import json
 import sys
 
+Cookie=None
+
 conn = http.client.HTTPSConnection("cloud.yandex.ru")
 payload = json.dumps({
   "message": "Привет!\nЯ Яндекс Спичк+ит.\nЯ могу превратить любой текст в речь.",
@@ -16,7 +18,7 @@ headers = {
   'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
   'Connection': 'keep-alive',
   'Content-Type': 'application/json',
-  'Cookie': 'yandexuid=4820047601666964160; yuidss=4820047601666964160; ymex=1982324160.yrts.1666964160#1982324160.yrtsi.1666964160; is_gdpr=0; gdpr=0; _ym_uid=1667193561507886098; _ym_d=1667193562; skid=3386796601667368099; _ga=GA1.2.1686453913.1668401924; my=YwA=; is_gdpr_b=CIy8DhCAlwEoAg==; yandex_login=andreas.svetlakov; font_loaded=YSv1; L=fCBoYlJHaFtIS2BnaXFVbAsLVWtBRll0LzcqEVcPGHg1BAhGNjAgKkI=.1669185429.15170.330354.51502f329c779b9bee36ebdd88a8dd19; ys=udn.cDphbmRyZWFzLnN2ZXRsYWtvdg%3D%3D#wprid.1671607457205048-15615117192998857305-sas3-0704-ded-sas-l7-balancer-8080-BAL-3748#c_chck.4076025427; yp=1684949848.szm.1:1920x1080:1920x969#1984545429.udn.cDphbmRyZWFzLnN2ZXRsYWtvdg%3D%3D#1986967458.pcs.1#1703143458.pgp.1_27860124#1672212253.mcv.0#1672212253.mcl.16d6fxq; yabs-frequency=/5/0000000000000000/swvnE0daUaO4IaSoR8W5ORFCL0HA9u6f6ZyhvZvR14eWlmdSQgKlb5G4IY3c_4pUHqc_KWHAG000/; i=lmSR0Tlgg6lS4aN5G+I09djP1Ae9QaJysnnfmDkGMjUzLxEl61ju77DFwevfvSm1QTCtLJJg81IPCbG15N+uWor2B3I=; _ym_isad=2; session-geo-data=eyJyZWdpb25OYW1lIjoi0KHRgtCw0LLRgNC%2B0L%2FQvtC70YwiLCJjb3VudHJ5TmFtZSI6ItCg0L7RgdGB0LjRjyIsInRpbWV6b25lIjoiRXVyb3BlL01vc2NvdyIsImxvY2FsZSI6eyJjb2RlIjoicnUtUlUiLCJuYW1lIjoi0KDQvtGB0YHQuNGPICjQoNGD0YHRgdC60LjQuSkiLCJsYW5nIjoicnUiLCJsYW5nTmFtZSI6ItCg0YPRgdGB0LrQuNC5IiwicmVnaW9uIjoicnUiLCJyZWdpb25OYW1lIjoi0KDQvtGB0YHQuNGPIiwiY3VycmVuY3kiOiJSVUIiLCJ0bGQiOiJydSIsIm9yZGVyIjoxLCJkZWZhdWx0Ijp0cnVlLCJsb2NhbCI6dHJ1ZX19; Session_id=3:1673597871.5.0.1669185265851:wpEAwg:35.1.2:1|1717287608.164.2.2:164|3:10264027.613980.8FeYrKM0730Wsfu8DcyGWJMU_u0; sessionid2=3:1673597871.5.0.1669185265851:wpEAwg:35.1.2:1|1717287608.164.2.2:164|3:10264027.613980.fakesign0000000000000000000; _ym_visorc=b; XSRF-TOKEN=81a944d5ac4de2213cd04cbe434bfb5f059c8a89%3A1673597891; _yasc=POihGQb455hEQU2FF0jqOqS2XcPVjhleRISSoHSLd6B4WS14eMkyPNavMbP+lXZ+Jw==',
+  'Cookie': Cookie,
   'Origin': 'https://cloud.yandex.ru',
   'Referer': 'https://cloud.yandex.ru/services/speechkit',
   'Sec-Fetch-Dest': 'empty',
@@ -26,7 +28,7 @@ headers = {
   'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
   'sec-ch-ua-mobile': '?0',
   'sec-ch-ua-platform': '"Windows"',
-  'x-csrf-token': '3d88b169fdd6b9214ae686f50c3e770ce731a0bd:1673597878'
+  'x-csrf-token': '---'
 }
 conn.request("POST", "/api/speechkit/tts", payload, headers)
 res = conn.getresponse()
