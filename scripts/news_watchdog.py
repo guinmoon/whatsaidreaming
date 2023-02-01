9390e5103763d77142b65dd6de8a77f890dcbc97
guinmoon <guinmoon@gmail.com>
2023-02-01 17:58:03 +0300

    watchdog upd

--- scripts/news_watchdog.py
+++ scripts/news_watchdog.py
@@ -6,4 +6,4 @@ if __name__=='__main__':
     __dir=os.path.dirname(os.path.realpath(__file__))    
     news_prompt = get_prompt_from_news("https://news.1777.ru/rss/yandex",only_check=True)
     if news_prompt is not None:
-        result = subprocess.run(["python3",f"{__dir}/think_about_something.py", "-n"],cwd=__dir)
+        result = subprocess.run(["python3.9",f"{__dir}/think_about_something.py", "-n"],cwd=__dir)
